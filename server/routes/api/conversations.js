f6e09910569a811ae30db8cf7477a9b080060d13
Minh Nguyen <minhtan16@hotmail.com>
2020-01-25 17:35:24 -0500

    get active users conversation and list of users talking to

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -4,17 +4,33 @@ const Message = require('../../models/Message');
 
 const router = express.Router();
 
-// Get all conversations of a user
+// Get active conversations
+router.get('/active', (req, res) => {
+    Conversation.find({ participants: '5e2bba66222c4f57b8e13b18' }) //req.user._id
+    .exec((err, conversations) => {
+      if (err) {
+        res.send({ error: err });
+        return next(err);
+      }
+      console.log('conversations: ' + conversations);
+      
+      conversations.forEach((conversation) => {
+            console.log('participants: ' + conversation.participants);
+      });
+      return res.status(200).json({ conversations });
+    });
+});
+
+// Call to get one conversation and it's most recent message
 router.get('/', (req, res) => {
-    Conversation.find({ participants: req.user._id })
-    .select('_id')
+    Conversation.find({ participants: req.user._id }) //req.user._id
     .exec((err, conversations) => {
       if (err) {
         res.send({ error: err });
         return next(err);
       }
 
-      // Set up empty array to hold conversations + most recent message
+      // Empty array for conversation and it's most recent message
       const fullConversations = [];
       conversations.forEach((conversation) => {
         Message.find({ conversationId: conversation._id })
@@ -59,43 +75,42 @@ router.get('/:conversationId', (req, res) => {
 
 // New Conversation
 router.post('/new/:recipientId', (req, res, next) => {
-    console.log('recipient:' + req.params.recipientId);
     if (!req.params.recipientId) {
         res.status(422).send({ error: 'Please choose a valid recipient for your message.' });
         return next();
-      }
+    }
     
-      if (!req.body.composedMessage) {
-        res.status(422).send({ error: 'Please enter a message.' });
-        return next();
-      }
+    if (!req.body.composedMessage) {
+    res.status(422).send({ error: 'Please enter a message.' });
+    return next();
+    }
     
-      const conversation = new Conversation({
-        // ['5e2bba66222c4f57b8e13b18', '5e2c770532edc50a94327c93']//
-        participants: [req.user._id, req.params.recipient]
-      });
+    const conversation = new Conversation({
     
-      conversation.save((err, newConversation) => {
+    participants: ['5e2bba66222c4f57b8e13b18', req.params.recipientId]//[req.user._id, req.params.recipient]
+    });
+    
+    conversation.save((err, newConversation) => {
         if (err) {
-          res.send({ error: err });
-          return next(err);
-        }
-    
-        const message = new Message({
-          conversationId: newConversation._id,
-          body: req.body.composedMessage,
-          author: req.user._id
-        });
-    
-        message.save((err, newMessage) => {
-          if (err) {
             res.send({ error: err });
             return next(err);
-          }
-    
-          return res.status(200).json({ message: 'Conversation started!', conversationId: conversation._id });
+        }
+
+        const message = new Message({
+            conversationId: newConversation._id,
+            body: req.body.composedMessage,
+            author: '5e2bba66222c4f57b8e13b18'//req.user._id
         });
-      });
+
+        message.save((err, newMessage) => {
+            if (err) {
+            res.send({ error: err });
+            return next(err);
+            }
+
+            return res.status(200).json({ message: 'Conversation started!', conversationId: conversation._id });
+        });
+    });
 });
 
 
@@ -105,7 +120,7 @@ router.post('/:conversationId', (req, res, next) => {
     const reply = new Message({
         conversationId: req.params.conversationId,
         body: req.body.composedMessage,
-        author: req.user._id
+        author: '5e2c770532edc50a94327c93'//
       });
     
       reply.save((err, sentReply) => {
