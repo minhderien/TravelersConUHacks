9f966789288032ee3f760850f08495a82fb76538
Minh Nguyen <minhtan16@hotmail.com>
2020-01-25 11:26:21 -0500

    hotfix bcrypt

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const User = require('../../models/User')
 const passport = require('passport');
 
